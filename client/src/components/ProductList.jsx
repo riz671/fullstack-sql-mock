de39e306ac10ce6129ebd44b9d74774fe702b237
Rizwan Choudhury <choudhuryrizwan97@gmail.com>
2020-06-14 13:16:24 -0700

    successfully renders product list

--- client/src/components/ProductList.jsx
+++ client/src/components/ProductList.jsx
@@ -5,8 +5,8 @@ const ProductList = (props) => {
   // iterate through products array that was passed
   return (
     <div className='product-list'>
-      {props.allProducts.map(product => {
-        return (<Products product={product} />)
+      {props.allProducts.map((product, index) => {
+        return (<Products product={product} key={index} />)
       })}
     </div>
   )
--- client/src/components/Products.jsx
+++ client/src/components/Products.jsx
@@ -1,8 +1,20 @@
 import React from 'react';
-  
-const Products = () => {
-   return(
+
+const Products = (props) => {
+  // destructure for ease of use
+  let { item_image, item, min_cost, curr_bid, ends_in } = props.product;
+
+  return (
     <div className='product-list-entry'>
+      <img src={item_image} alt="" />
+      <br />
+      <label>Item Name: {item}</label>
+      <br />
+      <label>Minimum Cost: ${min_cost}</label>
+      <br />
+      <label>Current Bid: ${curr_bid}</label>
+      <br />
+      <label>Bid Closes In: {ends_in} hours</label>
     </div>
   )
 }
