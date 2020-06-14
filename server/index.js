94088aa12278839450a0bc5a08b475afa69766fe
Rizwan Choudhury <choudhuryrizwan97@gmail.com>
2020-06-14 10:58:49 -0700

    added middleware

--- server/index.js
+++ server/index.js
@@ -1,27 +1,33 @@
 // Express Server
 // FIX ME :(
-  const express = require('express');
-  const path = require('path');
-  const router = require('./router');
+const express = require('express');
+const bodyparser = require('body-parser');
+const morgan = require('morgan');
+const cors = require('cors');
+const path = require('path');
+const router = require('./router.js');
 
-  const server = express();
-  
-  server.use('/', express.static(path.join(__dirname + '/../client/dist')));
-  
-  server.get('/name', (req, res) => {
-    res.status(200).send('This is your get request, modify this file to use your router!')
-  })
-  
-  server.post('/name', (req, res) => {
-    res.status(200).send('This is your post request, modify this file to use your router!')
-  })
-  
-  server.put('/name', (req, res) => {
-    res.status(200).send('This is your put request, modify this file to use your router!')
-  })
-  
-  server.delete('/name', (req, res) => {
-    res.status(200).send('This is your delete request, modify this file to use your router!')
-  })
+const server = express();
 
-  server.listen(port, () => console.log('Connected to port: 3000'))
+server.use('/', express.static(path.join(__dirname + '/../client/dist')));
+
+// should send data to the router to correct path.
+server.use('/', router);
+
+server.get('/name', (req, res) => {
+  res.status(200).send('This is your get request, modify this file to use your router!');
+});
+
+server.post('/name', (req, res) => {
+  res.status(200).send('This is your post request, modify this file to use your router!');
+});
+
+server.put('/name', (req, res) => {
+  res.status(200).send('This is your put request, modify this file to use your router!');
+});
+
+server.delete('/name', (req, res) => {
+  res.status(200).send('This is your delete request, modify this file to use your router!');
+});
+
+server.listen(port, () => console.log('Connected to port: 3000'));
