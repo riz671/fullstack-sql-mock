9c9d2daa85e96e9a67cf88c88dd2d251a05511cd
Rizwan Choudhury <choudhuryrizwan97@gmail.com>
2020-06-14 11:17:53 -0700

    create connection

--- db/index.js
+++ db/index.js
@@ -1,1 +1,13 @@
 // build your database
+const mysql = require('mysql');
+
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'helloKitty',
+  database: 'EBayDB'
+});
+
+connection.connect();
+
+module.exports = db;
