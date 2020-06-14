e180ad4baf4784efec3725343b2c74fa6cfc26b3
Rizwan Choudhury <choudhuryrizwan97@gmail.com>
2020-06-14 13:04:40 -0700

    implement product List

--- client/src/components/ProductList.jsx
+++ client/src/components/ProductList.jsx
@@ -2,8 +2,12 @@ import React from 'react';
 import Products from './Products';
 
 const ProductList = (props) => {
+  // iterate through products array that was passed
   return (
     <div className='product-list'>
+      {props.allProducts.map(product => {
+        return (<Products product={product} />)
+      })}
     </div>
   )
 }
